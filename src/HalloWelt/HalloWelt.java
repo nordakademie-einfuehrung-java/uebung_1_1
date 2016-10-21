2eeeec840a5948e70f549dcc3aafc6adae9c083d
janboatsch <jan.boettcher@nordakademie.de>
2016-10-21 09:47:58 +0200

    Hallo Welt

---
+++ src/HalloWelt/HalloWelt.java
@@ -0,0 +1,13 @@
+package HalloWelt;
+
+public class HalloWelt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hallo Säsch");
+		
+		
+	}
+
+}
