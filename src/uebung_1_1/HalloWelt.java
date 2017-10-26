a511ea440bdcc465bd417b37501badc8748ad6a2
15529 <15529@A001-08.nordakademie.intern>
2017-10-26 09:41:42 +0200

    Hallo Welt programmiert

---
+++ src/uebung_1_1/HalloWelt.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uebung_1_1;
+
+/**
+ * @author 15529
+ *
+ */
+public class HalloWelt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
